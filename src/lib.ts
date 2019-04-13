a379c799e81e1d62c0a3603fc7b32ecf3e4637ee
Ian K Smith <smithki707@gmail.com>
2019-04-12 19:31:00 -0600

    Major refactored aimed at simplifying the library

--- src/lib.ts
+++ src/lib.ts
@@ -4,11 +4,10 @@ import onChange from 'on-change';
 
 // --- Types & constants ---------------------------------------------------- //
 
-// Internal symbol accessors
 const namespaceSymbol = Symbol('namespaceSymbol');
 const isStorageProxy = Symbol('isStorageProxy');
-
-const namespaceRegex = /^\[(.+)\]:/;
+const storageTargetSymbol = Symbol('storageTargetSymbol');
+const storageProxyIntegrityKey = '__storageProxyIntegrity';
 
 /** Web storage targets: `localStorage` and `sessionStorage`. */
 export enum StorageTarget {
@@ -18,192 +17,72 @@ export enum StorageTarget {
 
 /** The object type created by `StorageProxy.createLocalStorage()` and `StorageProxy.createSessionStorage()`. */
 export type StorageProxy<TStorageDefinitions> = Partial<TStorageDefinitions> & {
-  [namespaceSymbol]: string | undefined;
-  [isStorageProxy]: true;
+  readonly [namespaceSymbol]: string;
+  readonly [isStorageProxy]: true;
+  readonly [storageTargetSymbol]: StorageTarget;
 };
-
-// Cache the proxies created by `onChange`. This prevents duplication of work in
-// the `StorageProxy` object and prevents an issue where proxies would compete
-// for access to web storage.
-const onChangeProxyCache: Map<string, any> = new Map();
 
 // --- Utilities ------------------------------------------------------------ //
 
 /**
- * Get all keys from the indicated web storage.
+ * Initializes the web storage interface. If no storage exists, we save an empty
+ * object.
  *
- * @param storageTarget - `localStorage` or `sessionStorage`.
+ * @param namespace - The namespace of the `StorageProxy` object.
+ * @param storageTarget - The web storage target (`localStorage` or `sessionStorage`).
  */
-function getStorageKeys(storageTarget: StorageTarget) {
-  const result: string[] = [];
-  for (let i = 0; i < window[storageTarget].length; i++) {
-    const key = window[storageTarget].key(i);
-    result.push(key!);
-  }
-  return result;
+function initDataStorage(namespace: string, storageTarget: StorageTarget) {
+  const data = window[storageTarget].getItem(namespace);
+  if (!data) window[storageTarget].setItem(namespace, JSON.stringify({}));
 }
 
 /**
- * Create an optionally-namespaced, storage-compatible key.
+ * Gets and parses data from web storage at the provided namespace.
  *
- * @param namespace - An optional namespace to use.
- * @param key - A key to identify the `localStorage` data.
+ * @param namespace - The namespace of the `StorageProxy` object.
+ * @param storageTarget - The web storage target (`localStorage` or `sessionStorage`).
+ *
+ * @return An object of arbitrary data from web storage.
  */
-function getNamespacedKey(namespace: string | undefined, key: string) {
-  if (isKeyNamespaced(key)) return key;
-  if (!namespace) return key;
-  return `[${namespace}]:${key}`;
+function getDataFromStorage(namespace: string, storageTarget: StorageTarget) {
+  const data = window[storageTarget].getItem(namespace);
+  return !!data ? JSON.parse(data) : {};
 }
 
 /**
- * Extracts the namespace from a given key
- *
- * @param key - Key to extract a namespace from.
- */
-function extractNamespaceFromKey(key: string): string | null {
-  const match = namespaceRegex.exec(key);
-  if (match) return match[1];
-  return null;
-}
-
-/**
- * Get the plain key from a namespaced key.
- *
- * @param key - The key to extract from.
- */
-function extractKeyFromNamespacedKey(key: string) {
-  return key.replace(namespaceRegex, '');
-}
-
-/**
- * Checks if the given key is namespaced.\
- *
- * @param key - The key to check.
- */
-function isKeyNamespaced(key: string) {
-  return !!extractNamespaceFromKey(key);
-}
-
-/**
- * Checks if the given key is valid for the given namespace.
- *
- * @param namespace - The namespace to check against.
- * @param key - The key to check.
- */
-function validateNamespace(namespace: string | undefined, key: string) {
-  if (isKeyNamespaced(key)) {
-    if (extractNamespaceFromKey(key) === namespace) return true;
-    return false;
-  }
-  if (!namespace) return true;
-  return false;
-}
-
-/**
- * Creates a proxy object that observes deeply for mutations and gets/sets web
- * storage accordingly.
+ * Creates a proxy object that observes deeply for mutations and syncs the
+ * desired web storage accordingly.
  *
  * @param storageTarget - Target `localStorage` or `sessionStorage` with the proxy.
  * @param namespace - An optional namespace to use.
+ *
+ * @return A `StorageProxy` object.
  */
 function createProxy<TStorageDefinitions extends any>(
   storageTarget: StorageTarget,
-  namespace?: string,
+  namespace: string,
 ): StorageProxy<TStorageDefinitions> {
-  const initialData: StorageProxy<TStorageDefinitions> = {
+  if (!namespace) throw new Error('[storage-proxy] Namespace cannot be an empty `string`, `undefined`, or `null`.');
+
+  initDataStorage(namespace, storageTarget);
+
+  const storageProxyMetadata = {
     [namespaceSymbol]: namespace,
     [isStorageProxy]: true,
-  } as any;
+    [storageTargetSymbol]: storageTarget,
+  };
 
-  // Return a proxy object
-  return new Proxy(initialData, {
+  const data = getDataFromStorage(namespace, storageTarget);
+  const proxyData = onChange(data, (path, value, prevValue) => {
+    if (value === prevValue) return;
+    window[storageTarget].setItem(namespace, JSON.stringify(proxyData));
+  });
+
+  return new Proxy(proxyData, {
     get: (target, prop, receiver) => {
-      if (typeof prop === 'string') {
-        const namespacedKey = getNamespacedKey(namespace, prop);
-        const data = window[storageTarget].getItem(namespacedKey);
-
-        if (data) {
-          const parsedData = JSON.parse(data);
-
-          if (typeof parsedData === 'object') {
-            // If we've already build a proxy, return the cached object.
-            if (onChangeProxyCache.has(namespacedKey)) {
-              return onChangeProxyCache.get(namespacedKey);
-            }
-
-            // Build a proxy with `onChange` to deeply observe object changes.
-            const proxyData = onChange(parsedData, () => {
-              window[storageTarget].setItem(
-                namespacedKey,
-                JSON.stringify(proxyData),
-              );
-            });
-
-            // Save the proxied data for future reference.
-            onChangeProxyCache.set(namespacedKey, proxyData);
-
-            return proxyData;
-          }
-
-          return parsedData;
-        }
-
-        return null;
-      }
-
-      return Reflect.get(target, prop, receiver);
-    },
-
-    set: (target, prop, value) => {
-      if (typeof prop === 'string') {
-        const namespacedKey = getNamespacedKey(namespace, prop);
-        if (typeof value === 'undefined') {
-          window[storageTarget].removeItem(namespacedKey);
-        } else {
-          window[storageTarget].setItem(namespacedKey, JSON.stringify(value));
-        }
-
-        // Reset the cached proxy for this key, if exists.
-        if (onChangeProxyCache.has(namespacedKey)) {
-          onChangeProxyCache.delete(namespacedKey);
-        }
-      }
-
-      return Reflect.set(target, prop, value);
-    },
-
-    ownKeys: target => {
-      return getStorageKeys(storageTarget)
-        .filter(key => validateNamespace(namespace, key))
-        .map(key => extractKeyFromNamespacedKey(key));
-    },
-
-    getOwnPropertyDescriptor: (target, prop) => {
-      if (typeof prop === 'string') {
-        const descriptor: PropertyDescriptor = {
-          configurable: true,
-          enumerable: true,
-          get: () => target[prop as any],
-          set: (val: any) => (target[prop as any] = val),
-        };
-        return descriptor;
-      }
-
-      return Reflect.getOwnPropertyDescriptor(target, prop);
-    },
-
-    deleteProperty: (target, prop) => {
-      if (typeof prop === 'string') {
-        const namespacedKey = getNamespacedKey(namespace, prop);
-        window[storageTarget].removeItem(namespacedKey);
-
-        // Reset the cached proxy for this key, if exists.
-        if (onChangeProxyCache.has(namespacedKey)) {
-          onChangeProxyCache.delete(namespacedKey);
-        }
-      }
-
-      return Reflect.deleteProperty(target, prop);
+      if (prop in storageProxyMetadata) return storageProxyMetadata[prop];
+      if (typeof proxyData[prop as any] === 'undefined') return null;
+      return proxyData[prop as any];
     },
   });
 }
@@ -219,10 +98,10 @@ export const StorageProxy = {
    * Creates a `localStorage` proxy object that can be used like a plain JS object.
    *
    * @param namespace - An optional namespace to prefix `localStorage` keys with.
+   *
+   * @return a `StorageProxy` object targeting `localStorage`.
    */
-  createLocalStorage<TStorageDefinitions extends any>(
-    namespace?: string,
-  ): StorageProxy<TStorageDefinitions> {
+  createLocalStorage<TStorageDefinitions extends any>(namespace: string): StorageProxy<TStorageDefinitions> {
     return createProxy<TStorageDefinitions>(StorageTarget.Local, namespace);
   },
 
@@ -230,41 +109,39 @@ export const StorageProxy = {
    * Creates a `sessionStorage` proxy object that can be used like a plain JS object.
    *
    * @param namespace - An optional namespace to prefix `sessionStorage` keys with.
+   *
+   * @return a `StorageProxy` object targeting `sessionStorage`.
    */
-  createSessionStorage<TStorageDefinitions extends any>(
-    namespace?: string,
-  ): StorageProxy<TStorageDefinitions> {
+  createSessionStorage<TStorageDefinitions extends any>(namespace: string): StorageProxy<TStorageDefinitions> {
     return createProxy<TStorageDefinitions>(StorageTarget.Session, namespace);
   },
 
   /**
-   * Checks a cache key in `localStorage` to verify whether the cache integrity
-   * is sound. This is useful for cache-busting.
+   * Checks a cache key in the given `StorageProxy` object and verifies whether
+   * the cache integrity is sound. This is handy for cache-busting
+   * `localStorage` and `sessionStorage`.
    *
    * @param storageProxy - The storage proxy object to verify.
-   * @param seed - A seed to check the cache integrity with.
+   * @param seed - A seed string to check the cache integrity against.
+   *
+   * @return `boolean` indicating whether the cache integrity is sound.
    */
-  verifyCache<TStorageProxy extends StorageProxy<any>>(
-    storageProxy: TStorageProxy,
-    seed: string,
-  ) {
+  verifyCache<TStorageProxy extends StorageProxy<any>>(storageProxy: TStorageProxy, seed: string) {
+    // Argument must be a `StorageProxy` object.
     if (!storageProxy[isStorageProxy]) {
-      throw new Error('Provided argument is not a `StorageProxy` object.');
-    }
-    const namespacedKey = getNamespacedKey(
-      storageProxy[namespaceSymbol],
-      'storageProxyIntegrity',
-    );
-    const existingSeed = window.localStorage.getItem(namespacedKey);
-    const decodedSeed = existingSeed
-      ? atob(JSON.parse(existingSeed))
-      : undefined;
-
-    if (decodedSeed) {
-      return decodedSeed === seed;
+      throw new Error('[storage-proxy] Provided argument is not a `StorageProxy` object.');
     }
 
-    window.localStorage.setItem(namespacedKey, JSON.stringify(btoa(seed)));
+    // Get a seed from the raw web storage data and decode it.
+    const data = getDataFromStorage(storageProxy[namespaceSymbol], storageProxy[storageTargetSymbol]);
+    const existingSeed = data[storageProxyIntegrityKey];
+    const decodedSeed = existingSeed ? atob(existingSeed) : undefined;
+
+    // If a seed exists, we will test against that.
+    if (decodedSeed) return decodedSeed === seed;
+
+    // Otherwise, we create a new, encoded seed...
+    storageProxy[storageProxyIntegrityKey] = btoa(seed);
     return true;
   },
 
@@ -275,21 +152,9 @@ export const StorageProxy = {
    *
    * @param storageProxy - The storage proxy object to clear.
    */
-  clearStorage<TStorageProxy extends StorageProxy<any>>(
-    storageProxy: TStorageProxy,
-  ) {
+  clearStorage<TStorageProxy extends StorageProxy<any>>(storageProxy: TStorageProxy) {
     for (const key of Object.keys(storageProxy)) {
       delete storageProxy[key];
     }
-  },
-
-  /**
-   * Create an namespaced, storage-compatible key.
-   *
-   * @param namespace - A namespace to use.
-   * @param key - A key to identify the `localStorage` data.
-   */
-  getNamespacedKey(namespace: string, key: string) {
-    return getNamespacedKey(namespace, key);
   },
 };
